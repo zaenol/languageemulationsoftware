def3fe48c17b10271fe32fdd9739c3c55af782d3
cmurockstar <cmurockstar@cc3120a8-0201-11df-8f7b-bfe16de6f99b>
2010-01-20 21:56:48 +0000

    slowing update

---
+++ trunk/LES/modelEditor/distortions/Anomic_correction_pauses.java
@@ -0,0 +1,31 @@
+package modelEditor.distortions;
+
+import org.w3c.dom.Document;
+
+import modelEditor.abstractClasses.AC_Distortion_BubbleUp;
+
+public class Anomic_correction_pauses extends AC_Distortion_BubbleUp {
+
+	public Anomic_correction_pauses(String id, boolean anomic,
+			boolean agrammatic, boolean distortion_error,
+			boolean distortion_correction) {
+		super(id, anomic, agrammatic, distortion_error, distortion_correction);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void getXML() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String[] parseString_local(String[] messages) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setValuesFromXML_local(Document dom) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
