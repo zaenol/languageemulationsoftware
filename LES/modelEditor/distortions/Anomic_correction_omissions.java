25ccef3ffa8e93a11536be8a134735866848d2df
cmurockstar <cmurockstar@cc3120a8-0201-11df-8f7b-bfe16de6f99b>
2010-01-22 06:53:08 +0000

    anomic gui

--- LES/modelEditor/distortions/Anomic_correction_omissions.java
+++ LES/modelEditor/distortions/Anomic_correction_omissions.java
@@ -1,14 +1,24 @@
 package modelEditor.distortions;
 
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
 import org.w3c.dom.Document;
 
 import modelEditor.abstractClasses.AC_Distortion_BubbleUp;
 
-public class Anomic_correction_omissions extends AC_Distortion_BubbleUp {
+public class Anomic_correction_omissions extends AC_Distortion_BubbleUp implements ChangeListener {
 
+	model m;
+	view v;
+	
 	public Anomic_correction_omissions(){
 		super("Omissions", true, false, false, true);
-		// TODO Auto-generated constructor stub
+		m = new model();
+		v = new view();
 	}
 
 	public Document getXML() {
@@ -24,6 +34,49 @@ public class Anomic_correction_omissions extends AC_Distortion_BubbleUp {
 	public void setValuesFromXML_local(Document dom) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public class view{
+		JSlider slider;
+		JLabel label;
+		
+		public view(){
+			label = new JLabel("Value of "+" 0%", JLabel.CENTER);
+			label.setAlignmentX(JPanel.CENTER_ALIGNMENT);
+			bodyPanel.add(label);
+			
+			slider = new JSlider(JSlider.HORIZONTAL, 0, 50, 0);
+			slider.setMajorTickSpacing(10);
+			slider.setMinorTickSpacing(2);
+			slider.setPaintTicks(true);
+			slider.setPaintLabels(true);
+			slider.addChangeListener(Anomic_correction_omissions.this);
+			bodyPanel.add(slider);
+		}
+		public void update(){
+			label.setText("Value of "+slider.getValue()+"%");
+		}
+		public int getSliderValue(){
+			return slider.getValue();
+		}
+	}
+	public class model{
+		int oldValue = 0;
+		public model(){
+			
+		}
+		public int getOldValue() {
+			return oldValue;
+		}
+		public void setOldValue(int oldValue) {
+			this.oldValue = oldValue;
+		}
+		
+	}
+	public void stateChanged(ChangeEvent e) {
+		v.update();
+		fireDoubleEvent(new Double(v.getSliderValue()-m.getOldValue()));
+		m.setOldValue(v.getSliderValue());		
 	}
 
 }
--- LES/modelEditor/distortions/Anomic_correction_semanticDescription.java
+++ LES/modelEditor/distortions/Anomic_correction_semanticDescription.java
@@ -1,15 +1,27 @@
 package modelEditor.distortions;
 
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
 import org.w3c.dom.Document;
 
 import modelEditor.abstractClasses.AC_Distortion_BubbleUp;
+import modelEditor.distortions.Anomic_correction_omissions.model;
+import modelEditor.distortions.Anomic_correction_omissions.view;
 
 public class Anomic_correction_semanticDescription extends
-		AC_Distortion_BubbleUp {
+		AC_Distortion_BubbleUp implements ChangeListener {
 
+			model m;
+			view v;
+			
 	public Anomic_correction_semanticDescription() {
 		super("Semantic Description", true, false, false, true);
-		// TODO Auto-generated constructor stub
+		m = new model();
+		v = new view();
 	}
 
 	public Document getXML() {
@@ -25,6 +37,49 @@ public class Anomic_correction_semanticDescription extends
 	public void setValuesFromXML_local(Document dom) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public class view{
+		JSlider slider;
+		JLabel label;
+		
+		public view(){
+			label = new JLabel("Value of "+" 0%", JLabel.CENTER);
+			label.setAlignmentX(JPanel.CENTER_ALIGNMENT);
+			bodyPanel.add(label);
+			
+			slider = new JSlider(JSlider.HORIZONTAL, 0, 50, 0);
+			slider.setMajorTickSpacing(10);
+			slider.setMinorTickSpacing(2);
+			slider.setPaintTicks(true);
+			slider.setPaintLabels(true);
+			slider.addChangeListener(Anomic_correction_semanticDescription.this);
+			bodyPanel.add(slider);
+		}
+		public void update(){
+			label.setText("Value of "+slider.getValue()+"%");
+		}
+		public int getSliderValue(){
+			return slider.getValue();
+		}
+	}
+	public class model{
+		int oldValue = 0;
+		public model(){
+			
+		}
+		public int getOldValue() {
+			return oldValue;
+		}
+		public void setOldValue(int oldValue) {
+			this.oldValue = oldValue;
+		}
+		
+	}
+	public void stateChanged(ChangeEvent e) {
+		v.update();
+		fireDoubleEvent(new Double(v.getSliderValue()-m.getOldValue()));
+		m.setOldValue(v.getSliderValue());		
 	}
 
 }
