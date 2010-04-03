7a7f8bd186f9239b1969c2263fe98fc71302ce0d
cmurockstar <cmurockstar@cc3120a8-0201-11df-8f7b-bfe16de6f99b>
2010-04-03 18:07:58 +0000

    fixed distortion order

--- trunk/LES/modelEditor/model/Model.java
+++ trunk/LES/modelEditor/model/Model.java
@@ -24,10 +24,12 @@ public class Model extends Model_findDistortions{
 		//Model_Message mm = new Model_Message(message);
 		Model_Message mm = message;
 		
+		mm = nonFluencyDist.parseString(mm);
+		mm = otherDist.parseString(mm); 
 		mm = wordDist.parseString(mm);
 		mm = inflectionDist.parseString(mm);
 		mm = functionDist.parseString(mm);
-		mm = nonFluencyDist.parseString(mm);
+		
 		
 		return mm;
 	}
