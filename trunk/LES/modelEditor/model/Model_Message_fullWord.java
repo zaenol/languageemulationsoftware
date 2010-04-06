28bc10a58b2a611d5bb4dcef1e6fea4678fcec5e
cmurockstar <cmurockstar@cc3120a8-0201-11df-8f7b-bfe16de6f99b>
2010-04-06 21:35:26 +0000

    pulled appart fullWords and posWords

---
+++ trunk/LES/modelEditor/model/Model_Message_fullWord.java
@@ -0,0 +1,145 @@
+package modelEditor.model;
+
+import java.util.ArrayList;
+
+
+import org.w3c.dom.Element;
+
+public class Model_Message_fullWord extends Model_Message_posWord{
+	protected class fullWord{
+		String oWord;
+		ArrayList<PosWord> posWords;
+		
+		public fullWord(String oWord, ArrayList<PosWord> posWords){
+			//System.out.println(oWord+":   ("+tokens+")  ("+tags+")");
+			this.oWord = oWord;
+			this.posWords = posWords;
+		}
+		
+		public String getWord_Original(){
+			return oWord;
+		}
+		
+		public boolean isNewMessageAfterFullWord(){
+			return posWords.get(posWords.size()-1).isNewMessageAfterWord();
+		}
+		
+		public ArrayList<String> getWord_PostDist(){
+			ArrayList<String> finalWord = new ArrayList<String>();
+			
+			boolean untouched = true;
+			for(int i=0; i<posWords.size() && untouched;i++){
+				if(posWords.get(i).isDistorted() || posWords.get(i).isNewMessageAfterWord()){
+					untouched = false;
+					
+					//if the last element has a new line, who cares... original word will suffice
+					if(i== posWords.size()-1 && !posWords.get(i).isDistorted() && posWords.get(i).isNewMessageAfterWord())
+						untouched = true;						
+				}
+					
+			}
+			
+			
+			if(untouched){
+				finalWord.add(this.getWord_Original());
+			}
+			else{	
+				
+				String currentMessage = "";
+				
+				PosWord _posWord = null;
+				
+				for(int i=0; i<posWords.size();i++){
+					PosWord posWord = posWords.get(i);
+					
+					if(_posWord == null){
+						if(i==posWords.size()-1){//last element
+							if(posWord.isDistorted())
+								currentMessage += posWord.getDistortedWord();
+							else
+								currentMessage += posWord.getTokenAsWord();
+						}else{//not last element
+							if(posWord.pos_isUnclassified() || !posWord.pos_isKnown()){//punctuation and other
+								currentMessage += posWord.getToken()+" ";
+								
+								if(posWord.isNewMessageAfterWord()){
+									finalWord.add(currentMessage);
+									currentMessage= "";
+								}
+								
+							}else{//is a real word
+								_posWord = posWord;
+							}		
+						}
+						
+					}else{//previous element exists!
+						
+						if(!_posWord.isDistorted() || !_posWord.isNewMessageAfterWord()){//previous word is NOT distorted NOR New line
+							
+							if(posWord.isDistorted()){//current word is distorted
+								currentMessage+=_posWord.getTokenAsWord()+" "+posWord.getDistortedWord();
+							}else{//current word is normal
+								currentMessage+=_posWord.getToken()+posWord.getToken();
+							}
+							
+						}else{//previous word IS distorted or has a new line
+							if(_posWord.isNewMessageAfterWord()){// new line after prior word
+								
+								if(_posWord.isDistorted())//prior word is distorted
+									currentMessage += _posWord.getDistortedWord();
+								else//prior word is not distorted
+									currentMessage += _posWord.getTokenAsWord();
+								
+								finalWord.add(currentMessage);
+								currentMessage= "";
+								
+								if(posWord.isDistorted()){//current word is distorted
+									currentMessage+=posWord.getDistortedWord();
+								}else{//current word is normal
+									currentMessage+=posWord.getTokenAsWord();
+								}
+								
+							}else{//no new line after prior word, use a space... also prior is distorted
+								
+								if(posWord.isDistorted()){//current word is distorted
+									currentMessage+=_posWord.getDistortedWord()+" "+posWord.getDistortedWord();
+								}else{//current word is normal
+									currentMessage+=_posWord.getDistortedWord()+" "+posWord.getTokenAsWord();
+								}
+								
+							}
+							
+						}
+						
+						if(posWord.isNewMessageAfterWord()){
+							finalWord.add(currentMessage);
+							currentMessage= "";
+						}
+						
+						_posWord = null;
+					}						
+					
+				}
+				
+				if(currentMessage.length()!=0)
+					finalWord.add(currentMessage);
+
+			}
+			
+			return finalWord;
+		}
+	
+		public Element getXML(){
+			Element word_element = dom.createElement("FullWord");
+			word_element.appendChild(makeElementWithTextBody("OriginalWord",getWord_Original()));
+			word_element.appendChild(makeElementWithIndextedChildren("FinalWord",getWord_PostDist(),"Word"));
+			
+			for(int i=0; i<posWords.size();i++){
+				word_element.appendChild(posWords.get(i).getXML());
+			}
+			
+			
+			return word_element;
+		}
+	}
+}
