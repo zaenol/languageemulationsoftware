c6e6efdc0e86c84931987d496b2018d4b3844b56
cmurockstar <cmurockstar@cc3120a8-0201-11df-8f7b-bfe16de6f99b>
2010-04-13 14:07:53 +0000

    bleh spell checker

---
+++ LES/modelEditor/distortions/spell/JoshSpeller.java
@@ -0,0 +1,16 @@
+package modelEditor.distortions.spell;
+
+import modelEditor.distortions.spell.event.SpellChecker;
+
+public class JoshSpeller {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpellChecker checker = new SpellChecker();
+		
+
+	}
+
+}
