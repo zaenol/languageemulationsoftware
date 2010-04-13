4d5c79616175de3749b8f03ed0b6d10af37e96ed
cmurockstar <cmurockstar@cc3120a8-0201-11df-8f7b-bfe16de6f99b>
2010-04-13 19:22:11 +0000

    stuggling w/ spelling

--- trunk/LES/modelEditor/distortions/spell/JoshSpeller.java
+++ trunk/LES/modelEditor/distortions/spell/JoshSpeller.java
@@ -1,15 +1,33 @@
 package modelEditor.distortions.spell;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import modelEditor.distortions.spell.engine.SpellDictionaryHashMap;
 import modelEditor.distortions.spell.event.SpellChecker;
 
 public class JoshSpeller {
+	
+	SpellChecker checker;
+	
+	public JoshSpeller(){
+		checker = new SpellChecker();
+		
+		try {
+			InputStream is = getClass().getResourceAsStream("/modelEditor/distortions/spell/english/english.0");
+	        checker.addDictionary(new SpellDictionaryHashMap(new InputStreamReader(is)));
+	      } catch (IOException ex) {
+	        ex.printStackTrace();
+	      }
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		SpellChecker checker = new SpellChecker();
-		
+		JoshSpeller js = new JoshSpeller();
+	      
 
 	}
 
