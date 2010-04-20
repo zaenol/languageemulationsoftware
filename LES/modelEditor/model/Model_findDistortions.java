43447544cf5e72672405d782d9f1c0e2a8ff12d3
cmurockstar <cmurockstar@cc3120a8-0201-11df-8f7b-bfe16de6f99b>
2010-04-20 20:16:15 +0000

    browses distortions!

--- LES/modelEditor/model/Model_findDistortions.java
+++ LES/modelEditor/model/Model_findDistortions.java
@@ -2,8 +2,18 @@ package modelEditor.model;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.lang.reflect.Constructor;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Vector;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import javax.swing.JFrame;
 
@@ -43,9 +53,30 @@ public abstract class Model_findDistortions {
      */
     protected void find_distortions(){
     	String dirName = "modelEditor/distortions";
+    	ArrayList<String> list = new ArrayList<String>();
+    	try {
+			String[] resources = getResourceListing(getClass(),dirName+"/");
+			for(String s: resources){
+				System.out.println(">> "+s);
+				if(s.contains(".java"))
+					list.add(s);
+			}
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+    	
+    	/*
+    	String dirName = "modelEditor/distortions";
         File dir = new File("./"+dirName+"/");
         
-        for(String c:dir.list(filter)){
+		
+        for(String c:dir.list(filter)){*/
+		for(String c:list){
         	try {
         		String forName = dirName.replace("/", ".")+"."+c.substring(0, c.length()-5);
         		//System.out.println(forName);
@@ -113,6 +144,56 @@ public abstract class Model_findDistortions {
 
 
     
-    
+    /**
+     * List directory contents for a resource folder. Not recursive.
+     * This is basically a brute-force implementation.
+     * Works for regular files and also JARs.
+     * 
+     * @author Greg Briggs
+     * @param clazz Any java class that lives in the same place as the resources you want.
+     * @param path Should end with "/", but not start with one.
+     * @return Just the name of each member item, not the full paths.
+     * @throws URISyntaxException 
+     * @throws IOException 
+     */
+    String[] getResourceListing(Class clazz, String path) throws URISyntaxException, IOException {
+        URL dirURL = clazz.getClassLoader().getResource(path);
+        if (dirURL != null && dirURL.getProtocol().equals("file")) {
+          /* A file path: easy enough */
+          return new File(dirURL.toURI()).list();
+        } 
+
+        if (dirURL == null) {
+          /* 
+           * In case of a jar file, we can't actually find a directory.
+           * Have to assume the same jar as clazz.
+           */
+          String me = clazz.getName().replace(".", "/")+".class";
+          dirURL = clazz.getClassLoader().getResource(me);
+        }
+        
+        if (dirURL.getProtocol().equals("jar")) {
+          /* A JAR path */
+          String jarPath = dirURL.getPath().substring(5, dirURL.getPath().indexOf("!")); //strip out only the JAR file
+          JarFile jar = new JarFile(URLDecoder.decode(jarPath, "UTF-8"));
+          Enumeration<JarEntry> entries = jar.entries(); //gives ALL entries in jar
+          Set<String> result = new HashSet<String>(); //avoid duplicates in case it is a subdirectory
+          while(entries.hasMoreElements()) {
+            String name = entries.nextElement().getName();
+            if (name.startsWith(path)) { //filter according to the path
+              String entry = name.substring(path.length());
+              int checkSubdir = entry.indexOf("/");
+              if (checkSubdir >= 0) {
+                // if it is a subdirectory, we just return the directory name
+                entry = entry.substring(0, checkSubdir);
+              }
+              result.add(entry);
+            }
+          }
+          return result.toArray(new String[result.size()]);
+        } 
+          
+        throw new UnsupportedOperationException("Cannot list files for URL "+dirURL);
+    }
 
 }
